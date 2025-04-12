3c01beeac555ffc37fc8d838c147994effdd632a
Yorqin Rahimboboyev <crossrick88@gmail.com>
2025-04-12 16:25:36 +0500

    glass nav

--- components/ScrollableNav.tsx
+++ components/ScrollableNav.tsx
@@ -3,12 +3,13 @@
 import { useCategoryStore } from "@/store/category";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function ScrollableNav({ categories }: { categories: any[] }) {
   const categoryActiveId = useCategoryStore((state) => state.activeId);
   const scrollRef = useRef<HTMLDivElement>(null);
   const buttonRefs = useRef<Record<string, HTMLButtonElement | null>>({});
+  const [scrolled, setScrolled] = useState(false);
 
   const scroll = (direction: "left" | "right") => {
     const container = scrollRef.current;
@@ -20,6 +21,15 @@ export default function ScrollableNav({ categories }: { categories: any[] }) {
       behavior: "smooth",
     });
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10); // когда прокрутили вниз на 10px
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   useEffect(() => {
     const activeBtn = buttonRefs.current[categoryActiveId];
@@ -38,7 +48,13 @@ export default function ScrollableNav({ categories }: { categories: any[] }) {
     }
   }, [categoryActiveId]);
   return (
-    <div className="bg-white rounded-b-xl shadow-lg py-2 px-2 flex flex-row justify-between max-sm:justify-center items-center sticky top-0 z-10">
+    <div
+      className={`sticky top-0 z-10 transition-all duration-300 px-2 py-2 flex flex-row justify-between rounded-b-lg max-sm:justify-center items-center ${
+        scrolled
+          ? "bg-white/50 backdrop-blur-md shadow-md"
+          : "bg-white shadow-lg"
+      }`}
+    >
       <button
         onClick={() => scroll("left")}
         className="z-10 bg-white rounded-full shadow p-2 max-sm:hidden"
@@ -48,9 +64,13 @@ export default function ScrollableNav({ categories }: { categories: any[] }) {
 
       <div
         ref={scrollRef}
-        className="rounded-full bg-[#EDE9F2] overflow-x-scroll max-sm:w-full md:max-w-[90%] scroll-hide"
+        className={`rounded-full  overflow-x-scroll max-sm:w-full md:max-w-[90%] scroll-hide ${
+          scrolled
+            ? "bg-[#EDE9F2]/40 backdrop-blur-md shadow-md"
+            : "bg-[#EDE9F2]"
+        }`}
       >
-        <div className="flex flex-row items-center whitespace-nowrap w-max py-[2px]">
+        <div className="flex flex-row items-center whitespace-nowrap w-max py-[2px] z-10">
           {categories.map((category: any) => (
             <Link
               ref={(el) => (buttonRefs.current[category.id] = el)}
